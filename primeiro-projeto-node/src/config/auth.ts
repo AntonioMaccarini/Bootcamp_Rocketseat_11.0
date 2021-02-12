77d9c3f9950b1e706f071c3251ee17b2d4f1498d
Antonio Maccarini <antoniomaccarini@alunos.utfpr.edu.br>
2021-02-12 11:28:23 -0300

    First commit

---
+++ primeiro-projeto-node/src/config/auth.ts
@@ -0,0 +1,7 @@
+
+export default {
+  jwt: {
+    secret: 'cc9f535301c95459513bf13ffe1a20e4',
+    expiresIn: '1d',
+  },
+};
---
+++ primeiro-projeto-node/src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,45 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+
+
+import uploadConfig from '../config/upload';
+import Users from '../models/Users';
+
+
+interface Request {
+  user_id: string
+  avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: Request): Promise<Users> {
+    const userRepository = getRepository(Users);
+
+    const user = await userRepository.findOne(user_id);
+
+    if (!user) {
+      throw new Error('Only authenticades users can change avatar');
+    }
+
+    if (user.avatar) {//Deletar avatar anterior
+
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+
+    user.avatar = avatarFilename;
+
+    await userRepository.save(user);
+
+    return user;
+
+  }
+}
+
+export default UpdateUserAvatarService;
